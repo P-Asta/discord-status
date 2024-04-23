b5282f3f3c47c080877cc1926e129b2bd2d9718a
5-23 <yhanbyeol6@gmail.com>
2024-04-23 21:41:21 +0900

    feat: add function

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,81 @@
+use std::{net::TcpListener, vec};
+
 use discord_rich_presence::{
     activity::{self, Assets, Timestamps},
     DiscordIpc, DiscordIpcClient,
 };
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut client = DiscordIpcClient::new("871348411356545057")?;
+pub fn main() {
+    let mut client: DiscordIpcClient = DiscordIpcClient::new("871348411356545057").unwrap();
+    client.connect().unwrap();
+    let status = Status {
+        title: "5-23",
+        detail: "5-23",
+        // large_img: "stone",
+        // large_text: "stone",
+        ..Default::default()
+    };
+    let client = status.start(&mut client).unwrap();
+}
+pub struct Button {
+    label: String,
+    url: String,
+}
 
-    client.connect()?;
-    client.set_activity(
-        activity::Activity::new()
-            .timestamps(Timestamps::new().start(1))
-            .state("foo")
-            .details("bar")
-            .assets(Assets::new().large_image("stone").large_text("돌돌이"))
-            .buttons(vec![activity::Button::new(
-                "test",
-                "https://github.com/5-23",
-            )]),
-    )?;
-    loop {
-        std::thread::sleep(std::time::Duration::from_secs(5));
+#[allow(non_snake_case)]
+fn Button(label: &str, url: &str) -> Button {
+    Button {
+        label: label.to_string(),
+        url: url.to_string(),
     }
-    // client.close()?;
+}
 
-    Ok(())
+pub struct Status {
+    title: &'static str,
+    detail: &'static str,
+    large_img: &'static str,
+    large_text: &'static str,
+    small_img: &'static str,
+    small_text: &'static str,
+    buttons: Vec<Button>,
+}
+
+impl Default for Status {
+    fn default() -> Self {
+        Self {
+            title: "1",
+            detail: "1",
+            large_img: "1",
+            large_text: "1",
+            small_img: "1",
+            small_text: "1",
+            buttons: vec![],
+        }
+    }
+}
+
+impl Status {
+    fn start(&self, client: &mut DiscordIpcClient) -> Result<(), Box<dyn std::error::Error>> {
+        let mut btns = vec![];
+        for btn in &self.buttons {
+            btns.push(activity::Button::new(&btn.label, &btn.url))
+        }
+
+        client.set_activity(
+            activity::Activity::new()
+                .timestamps(Timestamps::new().start(1))
+                .state(&self.title)
+                .details(&self.detail)
+                .assets(
+                    Assets::new()
+                        .large_image(&self.large_img)
+                        .large_text(&self.large_text)
+                        .small_image(&self.small_img)
+                        .small_text(&self.small_text),
+                ), // .buttons(btns),
+        )?;
+        // client.close()?;
+        loop {}
+        Ok(())
+    }
 }

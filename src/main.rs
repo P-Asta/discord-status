777b288cec56da799eb3a11da9a6e37678b2f4bc
5-23 <yhanbyeol6@gmail.com>
2024-04-25 10:37:09 +0900

    making: gui

--- src/main.rs
+++ src/main.rs
@@ -1,81 +1,55 @@
-use std::{net::TcpListener, vec};
-
-use discord_rich_presence::{
-    activity::{self, Assets, Timestamps},
-    DiscordIpc, DiscordIpcClient,
+use iced::{
+    self,
+    widget::{button, column, text, text_input},
+    Application, Color, Sandbox, Settings, Theme,
 };
-
-pub fn main() {
-    let mut client: DiscordIpcClient = DiscordIpcClient::new("871348411356545057").unwrap();
-    client.connect().unwrap();
-    let status = Status {
-        title: "5-23",
-        detail: "5-23",
-        // large_img: "stone",
-        // large_text: "stone",
-        ..Default::default()
-    };
-    let client = status.start(&mut client).unwrap();
-}
-pub struct Button {
-    label: String,
-    url: String,
+use iced_aw::{badge, color_picker};
+fn main() {
+    <App as Sandbox>::run(Settings::default()).unwrap();
 }
 
-#[allow(non_snake_case)]
-fn Button(label: &str, url: &str) -> Button {
-    Button {
-        label: label.to_string(),
-        url: url.to_string(),
+struct App;
+
+#[derive(Debug, Clone)]
+enum AppEvent {
+    Text(String),
+    BtnClick,
+}
+
+impl Sandbox for App {
+    type Message = AppEvent;
+
+    fn new() -> Self {
+        Self
     }
-}
-
-pub struct Status {
-    title: &'static str,
-    detail: &'static str,
-    large_img: &'static str,
-    large_text: &'static str,
-    small_img: &'static str,
-    small_text: &'static str,
-    buttons: Vec<Button>,
-}
-
-impl Default for Status {
-    fn default() -> Self {
-        Self {
-            title: "1",
-            detail: "1",
-            large_img: "1",
-            large_text: "1",
-            small_img: "1",
-            small_text: "1",
-            buttons: vec![],
+    fn update(&mut self, message: Self::Message) {
+        match message {
+            AppEvent::Text(t) => {
+                println!("Text: {}", t)
+            }
+            AppEvent::BtnClick => {
+                println!("Button clicked!");
+            }
         }
     }
-}
+    fn title(&self) -> String {
+        String::from("A cool application")
+    }
 
-impl Status {
-    fn start(&self, client: &mut DiscordIpcClient) -> Result<(), Box<dyn std::error::Error>> {
-        let mut btns = vec![];
-        for btn in &self.buttons {
-            btns.push(activity::Button::new(&btn.label, &btn.url))
-        }
-
-        client.set_activity(
-            activity::Activity::new()
-                .timestamps(Timestamps::new().start(1))
-                .state(&self.title)
-                .details(&self.detail)
-                .assets(
-                    Assets::new()
-                        .large_image(&self.large_img)
-                        .large_text(&self.large_text)
-                        .small_image(&self.small_img)
-                        .small_text(&self.small_text),
-                ), // .buttons(btns),
-        )?;
-        // client.close()?;
-        loop {}
-        Ok(())
+    fn theme(&self) -> iced::Theme {
+        iced::Theme::CatppuccinMocha
+    }
+    fn view(&self) -> iced::Element<'_, Self::Message> {
+        column![
+            text_input("client_id", "").on_input(AppEvent::Text),
+            text_input("title", "").on_input(AppEvent::Text),
+            text_input("description", "").on_input(AppEvent::Text),
+            text_input("large_image", "").on_input(AppEvent::Text),
+            text_input("large_text", "").on_input(AppEvent::Text),
+            text_input("small_image", "").on_input(AppEvent::Text),
+            text_input("small_text", "").on_input(AppEvent::Text),
+            button("sus").on_press(AppEvent::BtnClick)
+        ]
+        .into()
     }
 }
